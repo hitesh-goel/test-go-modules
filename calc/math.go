2b0ad072daa9189bfa23881dd2b483205d363992
Hitesh Goel <hitesh@udacity.com>
2020-04-30 19:49:11 +0530

    temp

--- calc/math.go
+++ calc/math.go
@@ -1,5 +1,8 @@
 package calc
 
+import "fmt"
+import "github.com/fatih/color"
+
 // returns sum of two integers
 func Add(numbers ...int) int {
     sum := 0
@@ -7,6 +10,8 @@ func Add(numbers ...int) int {
     for _, num := range numbers {
         sum = sum + num
     }
+
+    fmt.Println(color.RedString("test color"))
 
     return sum
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hitesh-goel/test-go-modules
 
 go 1.14
+
+require github.com/fatih/color v1.9.0 // indirect
